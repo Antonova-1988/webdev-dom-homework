014bfa180c6b34adcfc2b0f3f80df81bc3147e7f
Antonova-1988 <nizamytdenova@list.ru>
2025-11-16 18:14:11 +0500

    Обработка ошибок

--- modules/api.js
+++ modules/api.js
@@ -18,14 +18,53 @@ export const fetchComments = () => {
         })
 }
 
-export const postComment = (text, name) => {
+const maxRetriers = 3
+const addButton = document.querySelector('.add-form-button')
+const nameInput = document.querySelector('.add-form-name')
+const textInput = document.querySelector('.add-form-text')
+
+export const postComment = (text, name, retriers = maxRetriers) => {
     return fetch(host + '/comments', {
         method: 'POST',
         body: JSON.stringify({
             text,
             name,
+            forceError: false,
         }),
-    }).then(() => {
-        return fetchComments()
+    })
+        .then((response) => {
+            if (response.status === 201) {
+                return response.json()
+            } else {
+                if (response.status === 500) {
+                    if (retriers > 0) {
+                        return new Promise((resolve) =>
+                            setTimeout(resolve, 1000),
+                        ).then(() => {
+                            return postComment(text, name, maxRetriers - 1)
+                        })
+                    } else {
+                        throw new Error('Ошибка сервера')
+                    }
+                }
+
+                if (response.status === 400) {
+                    throw new Error('Неверный запрос')
+                }
+
+                throw new Error('Что-то пошло не так')
+            }
+        })
+        .then(() => {
+            return fetchComments()
+        })
+}
+
+const handlePostClick = () => {
+    postComment(textInput.value, nameInput.value).catch((error) => {
+        if (error.message === 'Ошибка сервера') {
+            handlePostClick()
+        }
     })
 }
+addButton.addEventListener('click', handlePostClick)
